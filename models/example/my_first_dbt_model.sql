b76845d8ab402506d390e01f51a6c48512c277d8
fivetran-eleshmistry <62256075+fivetran-eleshmistry@users.noreply.github.com>
2020-11-19 10:25:56 +0000

    Update my_first_dbt_model.sql

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -11,27 +11,15 @@
 
 with source_data as (
 
-    select 1 as id
+    select
+        account_id, full_name, name 
+
+    from netsuite.accounts
 
 )
 
 select *
 from source_data
-
-CREATE TEMP TABLE Example
-(
-  x INT64,
-  y STRING
-);
-
-INSERT INTO Example
-VALUES (5, 'foo');
-
-INSERT INTO Example
-VALUES (6, 'bar');
-
-SELECT *
-FROM Example;
 
 /*
     Uncomment the line below to remove records with null `id` values
