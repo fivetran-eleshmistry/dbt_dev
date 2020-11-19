7e50b356eda5c34205273d0a2fc56eb47cdf1b34
fivetran-eleshmistry <62256075+fivetran-eleshmistry@users.noreply.github.com>
2020-11-19 11:31:15 +0000

    Update my_second_dbt_model.sql

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,6 +1,6 @@
 
 -- Use the `ref` function to select from other models
 
-select *
+select account_id2, full_name, name
 from {{ ref('my_first_dbt_model') }}
 where account_id = 189 or account_id = 132
