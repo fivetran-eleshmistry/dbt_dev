3f1539ff261059d6ef019b7cd4b2f33e7575b544
fivetran-eleshmistry <62256075+fivetran-eleshmistry@users.noreply.github.com>
2020-11-19 11:31:27 +0000

    Update my_third_dbt_model.sql

--- models/example/my_third_dbt_model.sql
+++ models/example/my_third_dbt_model.sql
@@ -3,4 +3,4 @@
 
 select *
 from {{ ref('my_first_dbt_model') }} as table1
-JOIN {{ ref('my_second_dbt_model') }} as table2 ON table1.account_id = table2.account_id
+JOIN {{ ref('my_second_dbt_model') }} as table2 ON table1.account_id = table2.account_id2
