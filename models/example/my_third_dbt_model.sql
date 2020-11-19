de10d8ba5d43aea1d82b699fb103c910c3744932
fivetran-eleshmistry <62256075+fivetran-eleshmistry@users.noreply.github.com>
2020-11-19 11:24:52 +0000

    Update my_third_dbt_model.sql

--- models/example/my_third_dbt_model.sql
+++ models/example/my_third_dbt_model.sql
@@ -2,5 +2,5 @@
 -- Use the `ref` function to select from other models
 
 select *
-from {{ ref('my_first_dbt_model') }}
-JOIN {{ ref('my_second_dbt_model') }} ON my_first_dbt_model.account_id = my_second_dbt_model.account_id
+from {{ ref('my_first_dbt_model') }} as table1
+JOIN {{ ref('my_second_dbt_model') }} as table2 ON table1.account_id = table2.account_id
