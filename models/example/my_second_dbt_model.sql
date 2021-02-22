70b5e6bb55a94083c6b61576f5810b09887b3e90
fivetran-eleshmistry <62256075+fivetran-eleshmistry@users.noreply.github.com>
2021-02-22 16:18:41 +0000

    Update my_second_dbt_model.sql

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -3,4 +3,4 @@
 
 select account_id as account_id2, full_name, name
 from {{ ref('my_first_dbt_model') }}
-where account_id = '0014K000007L1qNQAS' or account_id = '0014K000007L1qOQAS'
+where id = '0014K000007L1qNQAS' or id = '0014K000007L1qOQAS'
