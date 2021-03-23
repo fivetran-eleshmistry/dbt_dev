7099502bb3f60ccf6f2e925cc7082c331551fa9a
fivetran-eleshmistry <62256075+fivetran-eleshmistry@users.noreply.github.com>
2021-03-23 17:45:46 +0000

    Delete my_second_dbt_model.sql

--- models/example/my_second_dbt_model.sql
+++
@@ -1,6 +0,0 @@
-
--- Use the `ref` function to select from other models
-
-select account_id as account_id2, full_name
-from {{ ref('my_first_dbt_model') }}
-where account_id = '1' or account_id = '5'
